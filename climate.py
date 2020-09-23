b3dabb493c5d96b9f65938482c11548ded66605f
David Bonnes <zxdavb@bonnes.me>
2020-09-23 22:20:52 +0100

    add controller

--- climate.py
+++ climate.py
@@ -34,19 +34,19 @@ async def async_setup_platform(
         return
 
     broker = hass.data[DOMAIN]["broker"]
-
-    new_zones = [z for z in broker.client.evo.zones if z not in broker.climates]
-    if not new_zones:
-        return
-
-    broker.climates += new_zones
     new_entities = []
 
-    for zone in [z for z in new_zones]:
+    if broker.client.evo not in broker.climates:
+        _LOGGER.warning("Found a Controller, id=%s", broker.client.evo)
+        new_entities.append(EvoController(broker, broker.client.evo))
+        broker.climates.append(broker.client.evo)
+
+    for zone in [z for z in broker.client.evo.zones if z not in broker.climates]:
         _LOGGER.warning(
             "Found a Zone (%s), id=%s, name=%s", zone.heating_type, zone.idx, zone.name,
         )
         new_entities.append(EvoZone(broker, zone))
+        broker.climates.append(zone)
 
     if new_entities:
         async_add_entities(new_entities, update_before_add=True)
@@ -84,18 +84,22 @@ class EvoZone(EvoZoneBase, ClimateEntity):
     @property
     def hvac_mode(self) -> Optional[str]:
         """Return hvac operation ie. heat, cool mode."""
-        if self._evo_device.mode:
-            if self._evo_device.mode["mode"] == "FollowSchedule":
-                return HVAC_MODE_AUTO
-            if self._evo_device.mode["mode"] == "PermanentOverride" and all(
-                (
-                    self.target_temperature is not None,
-                    self.min_temp is not None,
-                    self.target_temperature <= self.min_temp
-                )
-            ):
-                return HVAC_MODE_OFF
-            return HVAC_MODE_HEAT
+        if self._evo_device.mode is None:
+            return
+
+        if self._evo_device.mode["mode"] == "FollowSchedule":
+            return HVAC_MODE_AUTO
+
+        if self._evo_device.mode["mode"] == "PermanentOverride" and all(
+            (
+                self.target_temperature is not None,
+                self.min_temp is not None,
+                self.target_temperature <= self.min_temp
+            )
+        ):
+            return HVAC_MODE_OFF
+
+        return HVAC_MODE_HEAT
 
     @property
     def hvac_modes(self) -> List[str]:
@@ -130,7 +134,96 @@ class EvoZone(EvoZoneBase, ClimateEntity):
         """Set a Zone to one of its native operating modes."""
         if hvac_mode == HVAC_MODE_AUTO:  # FollowSchedule
             await self._evo_device.cancel_override()
+
         elif hvac_mode == HVAC_MODE_HEAT:  # TemporaryOverride
             await self._evo_device.set_override(mode="02", setpoint=25)
+
         else:  # HVAC_MODE_OFF, PermentOverride, temp = min
             await self._evo_device.frost_protect()
+
+
+class EvoController(EvoZoneBase, ClimateEntity):
+    """Base for a Honeywell Controller/Location."""
+
+    def __init__(self, evo_broker, evo_device) -> None:
+        """Initialize a Controller."""
+        super().__init__(evo_broker, evo_device)
+
+        self._unique_id = evo_device.id
+        self._icon = "mdi:thermostat"
+
+        self._supported_features = 0  # SUPPORT_PRESET_MODE
+
+    @property
+    def current_temperature(self) -> Optional[float]:
+        """Return the average current temperature of the heating Zones.
+
+        Controllers do not have a current temp, but one is expected by HA.
+        """
+        temps = [
+            z.temperature
+            for z in self._evo_device.zones
+            if z.temperature is not None
+        ]
+        return round(sum(temps) / len(temps), 1) if temps else None
+
+    @property
+    def device_state_attributes(self) -> Dict[str, Any]:
+        """Return the integration-specific state attributes."""
+        return {
+            **super().device_state_attributes,
+        }
+
+    @property
+    def hvac_action(self) -> Optional[str]:
+        """Return the current running hvac operation if supported."""
+        # if self._evo_device.mode is None:
+        #     return
+        # if self._evo_device.mode["system_mode"] == "HeatOff":
+        #     return CURRENT_HVAC_OFF
+        # if True:
+        #     return CURRENT_HVAC_HEAT
+        return CURRENT_HVAC_IDLE
+
+    @property
+    def hvac_mode(self) -> str:
+        """Return the current operating mode of a Controller."""
+        # tcs_mode = self._evo_tcs.systemModeStatus["mode"]
+        # return HVAC_MODE_OFF if tcs_mode == EVO_HEATOFF else HVAC_MODE_HEAT
+        pass
+
+    @property
+    def hvac_modes(self) -> List[str]:
+        """Return the list of available hvac operation modes."""
+        return [HVAC_MODE_AUTO, HVAC_MODE_OFF, HVAC_MODE_HEAT]
+
+    @property
+    def max_temp(self) -> None:
+        """Return None as Controllers don't have a target temperature."""
+        return None
+
+    @property
+    def min_temp(self) -> None:
+        """Return None as Controllers don't have a target temperature."""
+        return None
+
+    @property
+    def name(self) -> str:
+        return "Controller"
+
+    # @property
+    # def preset_mode(self) -> Optional[str]:
+    #     """Return the current preset mode, e.g., home, away, temp."""
+    #     return TCS_PRESET_TO_HA.get(self._evo_tcs.systemModeStatus["mode"])
+
+    # async def async_set_temperature(self, **kwargs) -> None:
+    #     """Raise exception as Controllers don't have a target temperature."""
+    #     raise NotImplementedError("Evohome Controllers don't have target temperatures.")
+
+    # async def async_set_hvac_mode(self, hvac_mode: str) -> None:
+    #     """Set an operating mode for a Controller."""
+    #     await self._set_tcs_mode(HA_HVAC_TO_TCS.get(hvac_mode))
+
+    # async def async_set_preset_mode(self, preset_mode: Optional[str]) -> None:
+    #     """Set the preset mode; if None, then revert to 'Auto' mode."""
+    #     await self._set_tcs_mode(HA_PRESET_TO_TCS.get(preset_mode, EVO_AUTO))
