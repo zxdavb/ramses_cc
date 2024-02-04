8ddf4e577f010ca1e6bd85aa0f35ed7a2d3c3a14
David Bonnes <zxdavb@bonnes.me>
2024-02-04 15:31:16 +0000

    lint

--- custom_components/ramses_cc/tests/tests_helpers.py
+++ custom_components/ramses_cc/tests/tests_helpers.py
@@ -7,7 +7,6 @@ from custom_components.ramses_cc.schemas import deep_merge
 
 
 def test_deep_merge() -> None:
-
     XXX = {"a": 10, "b": 20}
     YYY = {"a": 11, "c": 31}
 
@@ -18,13 +17,19 @@ def test_deep_merge() -> None:
     YYY = {"a": 11, "c": 31, "x": {"a": 71, "c": 91}}
 
     assert deep_merge(XXX, YYY) == {
-        'a': 10, 'c': 31, 'x': {'a': 70, 'c': 91, 'b': 80}, 'b': 20
+        "a": 10,
+        "c": 31,
+        "x": {"a": 70, "c": 91, "b": 80},
+        "b": 20,
     }
     assert deep_merge(YYY, XXX) == {
-        'a': 11, 'b': 20, 'x': {'a': 71, 'b': 80, 'c': 91}, 'c': 31
+        "a": 11,
+        "b": 20,
+        "x": {"a": 71, "b": 80, "c": 91},
+        "c": 31,
     }
 
     ZZZ = {"a": 10, "b": 20, "x": None}
 
     # assert deep_merge(x, z) == {}
-    assert deep_merge(ZZZ, XXX) == {'a': 10, 'b': 20, 'x': None}
+    assert deep_merge(ZZZ, XXX) == {"a": 10, "b": 20, "x": None}
