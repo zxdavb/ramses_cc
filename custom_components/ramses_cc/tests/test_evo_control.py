c2a5b86da6dfef9d405ee1426825d959c70fb751
David Bonnes <zxdavb@bonnes.me>
2024-02-15 02:58:45 +0000

    improve evo_control test

--- custom_components/ramses_cc/tests/test_evo_control.py
+++ custom_components/ramses_cc/tests/test_evo_control.py
@@ -10,6 +10,11 @@ from custom_components.ramses_cc.water_heater import WATER_HEATER_DESCRIPTIONS
 from ramses_rf.gateway import Gateway
 from ramses_rf.system import Evohome
 
+from homeassistant.components.binary_sensor import BinarySensorEntity
+from homeassistant.components.climate import PRESET_ECO, ClimateEntity, HVACMode
+from homeassistant.components.sensor import SensorEntity
+from homeassistant.components.water_heater import WaterHeaterEntity
+from homeassistant.const import STATE_OFF, STATE_ON
 from homeassistant.core import HomeAssistant
 
 TEST_DIR = Path(__file__).resolve().parent
@@ -17,7 +22,7 @@ INPUT_FILE = "evo_control.log"
 
 
 SCHEMA = {
-    "system": {"appliance_control": None},
+    "system": {"appliance_control": "13:120242"},
     "orphans": [],
     "stored_hotwater": {
         "sensor": "07:046947",
@@ -47,16 +52,19 @@ class MockRamsesBroker:
         self.hass = hass
 
 
-async def test_binary_sensors(hass: HomeAssistant) -> None:
-    """Test the namespace (i.e. entity_id) of entities used by EvoControl."""
-
+async def instantiate_entities(
+    hass: HomeAssistant,
+) -> tuple[
+    list[ClimateEntity],
+    list[WaterHeaterEntity],
+    list[BinarySensorEntity],
+    list[SensorEntity],
+]:
     with open(f"{TEST_DIR}/{INPUT_FILE}") as f:
         gwy: Gateway = Gateway(
             port_name=None, input_file=f, config={"disable_discovery": True}
         )
         await gwy.start()
-
-    assert gwy.devices
 
     broker: RamsesBroker = MockRamsesBroker(hass)
 
@@ -64,27 +72,27 @@ async def test_binary_sensors(hass: HomeAssistant) -> None:
     rf_climates = [s for s in gwy.systems if isinstance(s, Evohome)]
     rf_climates += [z for s in gwy.systems for z in s.zones if isinstance(s, Evohome)]
 
-    # water_heater entities (DHW)
-    rf_heaters = [s.dhw for s in gwy.systems if s.dhw if isinstance(s, Evohome)]
-
-    # binary_sensors & sensors entities
-    rf_devices = gwy.devices + rf_climates + rf_heaters
-
-    climates = [
+    climates: list[ClimateEntity] = [
         description.ramses_cc_class(broker, device, description)
         for device in rf_climates
         for description in CLIMATE_DESCRIPTIONS
         if isinstance(device, description.ramses_rf_class)
     ]
 
-    water_heaters = [
+    # water_heater entities (DHW)
+    rf_heaters = [s.dhw for s in gwy.systems if s.dhw if isinstance(s, Evohome)]
+
+    water_heaters: list[WaterHeaterEntity] = [
         description.ramses_cc_class(broker, device, description)
         for device in rf_heaters
         for description in WATER_HEATER_DESCRIPTIONS
         if isinstance(device, description.ramses_rf_class)
     ]
 
-    binary_sensors = [
+    # binary_sensors & sensors entities
+    rf_devices = gwy.devices + rf_climates + rf_heaters
+
+    binary_sensors: list[BinarySensorEntity] = [
         description.ramses_cc_class(broker, device, description)
         for device in rf_devices
         for description in BINARY_SENSOR_DESCRIPTIONS
@@ -92,7 +100,7 @@ async def test_binary_sensors(hass: HomeAssistant) -> None:
         and hasattr(device, description.ramses_rf_attr)
     ]
 
-    sensors = [
+    sensors: list[SensorEntity] = [
         description.ramses_cc_class(broker, device, description)
         for device in rf_devices
         for description in SENSOR_DESCRIPTIONS
@@ -100,51 +108,102 @@ async def test_binary_sensors(hass: HomeAssistant) -> None:
         and hasattr(device, description.ramses_rf_attr)
     ]
 
+    return climates, water_heaters, binary_sensors, sensors
+
+
+async def test_namespace(hass: HomeAssistant) -> None:
+    """Test the namespace (i.e. entity_id) of entities used by EvoControl."""
+
     # The intention here is check the namespace used by EvoControl
+    climates, water_heaters, binary_sensors, sensors = await instantiate_entities(hass)
 
-    # binary_sensor.${cid}_status
-    id = "binary_sensor.01:145038_status"
-    evo = [e for e in binary_sensors if e.entity_id == id][0]
+    #
+    # evo_control uses: binary_sensor.${cid}_status
+    ctl_id = "01:145038"  # via a webform, from the user
+    id = f"binary_sensor.{ctl_id}_status"
 
-    # the working schema
-    schema = evo.extra_state_attributes["working_schema"]
+    binary: BinarySensorEntity = [e for e in binary_sensors if e.entity_id == id][0]
+    assert binary.unique_id == f"{ctl_id}-status"
+    assert binary.state in (STATE_ON, STATE_OFF, None)
+
+    # evo_control uses: the working_schema
+    schema = binary.extra_state_attributes["working_schema"]
     assert schema["stored_hotwater"] == SCHEMA["stored_hotwater"]
     assert schema["zones"] == SCHEMA["zones"]
 
-    # binary_sensor.${i}_battery_low
-    for id in ("binary_sensor.34:092243_battery_low",):
-        assert [e for e in binary_sensors if e.entity_id == id]
+    #
+    # evo_control uses: binary_sensor.${i}_battery_low
+    for dev_id in ("07:046947", "22:140285", "34:092243"):  # via walking the schema
+        id = f"binary_sensor.{dev_id}_battery_low"
 
-    # binary_sensor.${cid}_${haZid}_window_open
-    for id in (
-        "binary_sensor.01:145038_02_window_open",
-        "binary_sensor.01:145038_0A_window_open",
-    ):
-        assert [e for e in binary_sensors if e.entity_id == id]
+        binary = [e for e in binary_sensors if e.entity_id == id][0]
+        assert binary.unique_id == f"{dev_id}-battery_low"
+        assert binary.state in (STATE_ON, STATE_OFF, None)
 
-    # sensor.${cid}_heat_demand
-    for id in ("sensor.01:145038_heat_demand",):
-        assert [e for e in sensors if e.entity_id == id]
+        battery_level = binary.extra_state_attributes["battery_level"]
+        assert battery_level is None or 0.0 < battery_level < 1.0  # FIXME
 
-    # sensor.${dhwRelayId}_relay_demand
-    for id in ("sensor.01:145038_HW_heat_demand",):
-        assert [e for e in sensors if e.entity_id == id]
+    # evo_control uses: binary_sensor.${cid}_${haZid}_window_open
+    for zon_idx in ("02", "0A"):  # via walking the schema
+        id = f"binary_sensor.{ctl_id}_{zon_idx}_window_open"
 
-    # sensor.${cid}_${haZid}_heat_demand
-    for id in (
-        "sensor.01:145038_0A_heat_demand",
-        "sensor.01:145038_0A_heat_demand",
-    ):
-        assert [e for e in sensors if e.entity_id == id]
+        binary = [e for e in binary_sensors if e.entity_id == id][0]
+        assert binary.unique_id == f"{ctl_id}_{zon_idx}-window_open"
+        assert binary.state in (STATE_ON, STATE_OFF, None)
 
-    # climate.${cid}
-    for id in ("climate.01:145038",):
-        assert [e for e in climates if e.entity_id == id]
+    #
+    # evo_control uses: sensor.${cid}_heat_demand
+    id = f"sensor.{ctl_id}_heat_demand"
 
-    # climate.${cid}_${haZid}
-    for id in ("climate.01:145038_02", "climate.01:145038_0A"):
-        assert [e for e in climates if e.entity_id == id]
+    sensor: SensorEntity = [e for e in sensors if e.entity_id == id][0]
+    assert sensor.unique_id == f"{ctl_id}-heat_demand"
+    assert sensor.state is None or 0.0 <= sensor.state <= 100.0
 
-    # water_heater.${cid}_hw
-    for id in ("water_heater.01:145038_HW",):
-        assert [e for e in water_heaters if e.entity_id == id]
+    # evo_control uses: sensor.${dhwRelayId}_relay_demand
+    bdr_id = "13:120242"  # via walking the schema
+    id = f"binary_sensor.{bdr_id}_active"
+
+    binary = [e for e in binary_sensors if e.entity_id == id][0]
+    assert binary.unique_id == f"{bdr_id}-active"
+    assert binary.state in (STATE_ON, STATE_OFF, None)
+
+    id = f"sensor.{bdr_id}_relay_demand"
+
+    sensor = [e for e in sensors if e.entity_id == id][0]
+    assert sensor.unique_id == f"{bdr_id}-relay_demand"
+    assert sensor.state is None or 0.0 <= sensor.state <= 100.0
+
+    # evo_control uses: sensor.${cid}_${haZid}_heat_demand
+    for zon_idx in ("02", "0A", "HW"):  # via walking the schema
+        id = f"sensor.{ctl_id}_{zon_idx}_heat_demand"
+
+        sensor = [e for e in sensors if e.entity_id == id][0]
+        assert sensor.unique_id == f"{ctl_id}_{zon_idx}-heat_demand"
+        assert sensor.state is None or 0.0 <= sensor.state <= 100.0
+
+    #
+    # evo_control uses: climate.${cid}
+    id = f"climate.{ctl_id}"  # via a webform, from the user
+
+    climate: ClimateEntity = [e for e in climates if e.entity_id == id][0]
+    assert climate.state == HVACMode.HEAT
+    assert climate.preset_mode == PRESET_ECO
+    climate.extra_state_attributes["system_mode"] = {
+        "system_mode": "eco_boost",
+        "until": "2022-03-06T14:44:00",
+    }
+
+    # evo_control uses: climate.${cid}_${haZid}
+    for zon_idx in ("02", "0A"):  # via walking the schema
+        id = f"climate.01:145038_{zon_idx}"
+
+        climate = [e for e in climates if e.entity_id == id][0]
+        assert isinstance(climate.current_temperature, float)
+
+    # evo_control uses: water_heater.${cid}_hw
+    id = f"water_heater.{ctl_id}_HW"  # via walking the schema
+
+    heater: WaterHeaterEntity = [e for e in water_heaters if e.entity_id == id][0]
+    assert isinstance(heater.current_temperature, float)
+
+    assert True
