4143bf2ecb1fcb3ed832cebafef3f9c61d279019
David Bonnes <zxdavb@bonnes.me>
2024-02-22 18:59:57 +0000

    tweak test

--- tests/test_init.py
+++ tests/test_init.py
@@ -1,7 +1,8 @@
 """Test the setup."""
 
-import asyncio
 from collections.abc import AsyncGenerator
+from typing import Final
+from unittest.mock import patch
 
 from custom_components.ramses_cc import DOMAIN, RamsesBroker
 import pytest
@@ -11,6 +12,9 @@ from homeassistant.config_entries import ConfigEntryState
 from homeassistant.core import HomeAssistant
 from homeassistant.setup import async_setup_component
 from tests.virtual_rf import VirtualRf
+
+# patched constants
+_CALL_LATER_DELAY: Final = 0  # from: custom_components.ramses_cc.broker.py
 
 CONFIG = {
     "serial_port": "/dev/ttyACM0",
@@ -35,7 +39,6 @@ async def test_setup_entry(hass: HomeAssistant, rf: VirtualRf) -> None:
 
     rf = VirtualRf(1)  # TODO: fixture is not working!
     rf.set_gateway(rf.ports[0], "18:000730")  # , fw_type=HgiFwTypes.HGI_80)
-
     config = {"serial_port": {"port_name": rf.ports[0]}}
 
     assert len(hass.config_entries.async_entries(DOMAIN)) == 0
@@ -48,30 +51,26 @@ async def test_setup_entry(hass: HomeAssistant, rf: VirtualRf) -> None:
     entry.add_to_hass(hass)
     assert len(hass.config_entries.async_entries(DOMAIN)) == 1
 
-    # mocked to cut out serial/MQTT
-    # th patch("custom_components.ramses_cc.RamsesBroker", return_value=AsyncMock()):
-    # with patch.object(RamsesBroker, "_create_client", return_value=23):
-    result = await hass.config_entries.async_setup(entry.entry_id)
-    assert result  # False if SETUP_ERROR
+    # mocked to avoid: Lingering timer after job <Job call_later 5...
+    with patch(
+        "custom_components.ramses_cc.broker._CALL_LATER_DELAY", _CALL_LATER_DELAY
+    ):
+        assert await hass.config_entries.async_setup(entry.entry_id)
+        # await hass.async_block_till_done()
 
-    await hass.async_block_till_done()
-    assert entry.state is ConfigEntryState.LOADED  # or: SETUP_ERROR
+    entries = hass.config_entries.async_entries(DOMAIN)
+    assert len(entries) == 1
 
-    # assert len(hass.states.async_entity_ids(SENSOR_DOMAIN)) == 1
+    assert entry is entries[0]
+    assert entry.state is ConfigEntryState.LOADED
 
     assert hass.data["setup_tasks"] == {}
     assert isinstance(hass.data[DOMAIN][entry.entry_id], RamsesBroker)
 
-    assert isinstance(hass.data[DOMAIN][entry.entry_id], RamsesBroker)
-
-    await hass.async_block_till_done()  # TODO: not quiescing (why?)...
-    await asyncio.sleep(5)  # #           TODO: workaround
+    # assert len(hass.states.async_entity_ids(SENSOR_DOMAIN)) == 1
 
     assert await hass.config_entries.async_unload(entry.entry_id)
-    await hass.async_block_till_done()
-
     assert await hass.config_entries.async_remove(entry.entry_id)
-    await hass.async_block_till_done()
 
     # hass.stop()  # not needed?
     # await hass.async_block_till_done()
@@ -80,45 +79,34 @@ async def test_setup_entry(hass: HomeAssistant, rf: VirtualRf) -> None:
 
 
 async def test_setup_import(hass: HomeAssistant, rf: VirtualRf) -> None:
-    """Test setup of ramses_cc via config file."""
+    """Test setup of ramses_cc via importing a configuration."""
 
     rf = VirtualRf(1)  # TODO: fixture is not working!
     rf.set_gateway(rf.ports[0], "18:000730")  # , fw_type=HgiFwTypes.HGI_80)
+    config = {"serial_port": {"port_name": rf.ports[0]}}
 
-    config = {"serial_port": rf.ports[0]}
+    assert len(hass.config_entries.async_entries(DOMAIN)) == 0
 
-    # mocked to cut out serial/MQTT
-    # with patch("ramses_rf.gateway.is_hgi80", return_value=None):
-    assert await async_setup_component(hass, DOMAIN, {DOMAIN: config})  # True
-
-    # Setup failed for 'ramses_cc': Integration not found.
-    # assert hass.data["setup_tasks"][DOMAIN].done()
-    # assert hass.data["setup_tasks"][DOMAIN].result() is True
-    # hass.data["persistent_notification"]["invalid_config"]["message"]
-
-    # custom_components.ramses_cc.broker:broker.py:75 Config = {'ramses_rf': {}, 'serial_port': {'port_name': '/dev/ttyACM0'}, 'schema': {}}
-    # custom_components.ramses_cc.broker:broker.py:99 Storage = {}
-    # homeassistant.config_entries:config_entries.py:485 Config entry 'RAMSES RF' for ramses_cc integration not ready yet:
-    #   There is a problem with the serial port: Unable to find /dev/ttyACM0; Retrying in 10 seconds
-
-    # await hass.async_block_till_done()  # not needed?
-    # if not hass.data[DOMAIN]:  # in ramses_cc.async_setup_entry()
-    #     pass  # something went wrong
+    # mocked to avoid: Lingering timer after job <Job call_later 5...
+    with patch(
+        "custom_components.ramses_cc.broker._CALL_LATER_DELAY", _CALL_LATER_DELAY
+    ):
+        assert await async_setup_component(hass, DOMAIN, {DOMAIN: config})  # True
+        # await hass.async_block_till_done()
 
     entries = hass.config_entries.async_entries(DOMAIN)
     assert len(entries) == 1
 
     entry = entries[0]
+    assert entry.state is ConfigEntryState.LOADED
+
+    assert hass.data["setup_tasks"] == {}
     assert isinstance(hass.data[DOMAIN][entry.entry_id], RamsesBroker)
 
-    await hass.async_block_till_done()  # TODO: not quiescing (why?)...
-    await asyncio.sleep(5)  # #           TODO: workaround
+    # assert len(hass.states.async_entity_ids(SENSOR_DOMAIN)) == 1
 
     assert await hass.config_entries.async_unload(entry.entry_id)
-    await hass.async_block_till_done()
-
     assert await hass.config_entries.async_remove(entry.entry_id)
-    await hass.async_block_till_done()
 
     # hass.stop()  # not needed?
     # await hass.async_block_till_done()
