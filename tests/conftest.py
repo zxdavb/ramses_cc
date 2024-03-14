dd829f3f7d8d91a1c4b3b02a42ab0cd5e946d853
David Bonnes <zxdavb@bonnes.me>
2024-03-14 09:42:17 +0000

    fixtures to be tolerant of new Transports (CP)

--- tests/conftest.py
+++ tests/conftest.py
@@ -6,3 +6,10 @@ import pytest
 @pytest.fixture(autouse=True)
 def auto_enable_custom_integrations(enable_custom_integrations: pytest.fixture):  # type: ignore[no-untyped-def]
     yield
+
+
+@pytest.fixture(autouse=True)
+def patches_for_tests(monkeypatch: pytest.MonkeyPatch):
+    monkeypatch.setattr("ramses_tx.protocol._DBG_DISABLE_IMPERSONATION_ALERTS", True)
+    monkeypatch.setattr("ramses_tx.transport._DBG_DISABLE_DUTY_CYCLE_LIMIT", True)
+    monkeypatch.setattr("ramses_tx.transport._GAP_BETWEEN_WRITES", 0)
