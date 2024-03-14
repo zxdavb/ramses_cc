962443cee3221eef599670cf09c9112c340956a9
David Bonnes <zxdavb@bonnes.me>
2024-03-14 09:50:09 +0000

    make tests tolerant of new Transport

--- tests/conftest.py
+++ tests/conftest.py
@@ -10,6 +10,17 @@ def auto_enable_custom_integrations(enable_custom_integrations: pytest.fixture):
 
 @pytest.fixture(autouse=True)
 def patches_for_tests(monkeypatch: pytest.MonkeyPatch):
-    monkeypatch.setattr("ramses_tx.protocol._DBG_DISABLE_IMPERSONATION_ALERTS", True)
-    monkeypatch.setattr("ramses_tx.transport._DBG_DISABLE_DUTY_CYCLE_LIMIT", True)
-    monkeypatch.setattr("ramses_tx.transport._GAP_BETWEEN_WRITES", 0)
+    try:
+        # refactored protocol / transport modules
+        monkeypatch.setattr(
+            "ramses_tx.protocol._DBG_DISABLE_IMPERSONATION_ALERTS", True
+        )
+        monkeypatch.setattr("ramses_tx.transport._DBG_DISABLE_DUTY_CYCLE_LIMIT", True)
+        monkeypatch.setattr("ramses_tx.transport._GAP_BETWEEN_WRITES", 0)
+    except AttributeError:
+        # original protocol / transport modules
+        monkeypatch.setattr(
+            "ramses_tx.protocol._DBG_DISABLE_IMPERSONATION_ALERTS", True
+        )
+        monkeypatch.setattr("ramses_tx.protocol._DBG_DISABLE_DUTY_CYCLE_LIMIT", True)
+        monkeypatch.setattr("ramses_tx.protocol._GAP_BETWEEN_WRITES", 0)
