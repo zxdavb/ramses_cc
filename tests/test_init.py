1173e219b390f891fc92ba0e22bb28014faa042d
David Bonnes <zxdavb@bonnes.me>
2024-05-06 20:37:29 +0100

    fix GWY address

--- tests/test_init.py
+++ tests/test_init.py
@@ -43,7 +43,7 @@ async def rf(hass: HomeAssistant) -> AsyncGenerator[Any, None]:
     """Utilize a virtual evofw3-compatible gateway."""
 
     rf = VirtualRf(2)
-    rf.set_gateway(rf.ports[0], "18:000730")
+    rf.set_gateway(rf.ports[0], "18:006402")
 
     with patch("ramses_tx.transport.comports", rf.comports):
         try:
--- tests/test_services.py
+++ tests/test_services.py
@@ -257,7 +257,7 @@ async def entry(hass: HomeAssistant) -> AsyncGenerator[ConfigEntry, None]:
 
     # Utilize a virtual evofw3-compatible gateway
     rf = VirtualRf(2)
-    rf.set_gateway(rf.ports[0], "18:000730")
+    rf.set_gateway(rf.ports[0], "18:006402")
 
     with patch(
         "custom_components.ramses_cc.broker._CALL_LATER_DELAY", _CALL_LATER_DELAY
